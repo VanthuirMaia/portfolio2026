5d42c39dab620ba5539a2593ff2b1f8e411000ac
VanthuirMaia <vanmaiasf@gmail.com>
2026-02-08 20:12:18 -0300

    feat: add personal info and skills section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://VanthuirMaia.github.io",
+  site: "https://vanthuirmaia.github.io",
   base: "/portfolio2026",
   integrations: [mdx()],
   output: "static",
